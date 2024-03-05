fbd902ba0a3c5ba3f708ab356fa925ecab823567
roshni-v <34416927+roshni-v@users.noreply.github.com>
2024-03-04 20:25:00 -0800

    part b finished

--- packages/express-backend/src/index.ts
+++ packages/express-backend/src/index.ts
@@ -1,12 +1,15 @@
 // src/index.ts
 import express, { Request, Response } from "express";
 import cors from "cors";
+import { connect } from "./mongoConnect";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+connect("ReadingClassics"); // use your own db name here
 
 app.get("/hello", (req: Request, res: Response) => {
     res.send("Hello, World");
